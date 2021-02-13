98466701123b5ab46ef3b3d8867e863f076fbc28
Kyle <kylefredin@gmail.com>
2021-02-13 15:52:22 -0700

    add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 import { SERVER_PORT } from "./constants";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Movies")
+    .setDescription("The Movie API")
+    .setVersion("1.0")
+    .addTag("movies")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(Number(SERVER_PORT));
 }
