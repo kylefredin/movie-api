d05b3cfd6155c73de490795f910ef2c24a07da1b
Kyle <kylefredin@gmail.com>
2021-02-21 08:20:06 -0700

    add create/update methods for movies

--- src/modules/movies/controller.ts
+++ src/modules/movies/controller.ts
@@ -1,20 +1,36 @@
 import {
+  Body,
   Controller,
   Get,
   HttpException,
   HttpStatus,
   Param,
+  Post,
+  Put,
   Query,
 } from "@nestjs/common";
 import { MovieDto } from "../../dto/movie.dto";
 import { MoviesDto } from "../../dto/movies.dto";
 import { PaginationDto } from "../../dto/pagination.dto";
+import { Movie } from "../../entity/Movie";
 import { MovieService } from "./service";
 
+/**
+ * API controller for Movie routes
+ */
 @Controller("movies")
 class MovieController {
+  /**
+   * @param {MovieService} movieService
+   */
   constructor(private movieService: MovieService) {}
 
+  /**
+   * GET /movies route handler
+   *
+   * @param {PaginationDto} query
+   * @return {Promise<MoviesDto>}
+   */
   @Get()
   async findAll(@Query() query: PaginationDto): Promise<MoviesDto> {
     const response = new MoviesDto();
@@ -28,6 +44,12 @@ class MovieController {
     return response;
   }
 
+  /**
+   * GET /movies/:id route handler
+   *
+   * @param {number} id
+   * @return {Promise<MovieDto>}
+   */
   @Get(":id")
   async findOne(@Param("id") id: number): Promise<MovieDto> {
     const movie = await this.movieService.findOne(id);
@@ -39,6 +61,47 @@ class MovieController {
     const response = new MovieDto();
 
     response.movie = movie;
+
+    return response;
+  }
+
+  /**
+   * POST /movies route handler
+   *
+   * @param {Movie} body
+   * @return {Promise<MovieDto>}
+   */
+  @Post()
+  async create(@Body() body: Movie): Promise<MovieDto> {
+    const movie = await this.movieService.create(body);
+
+    const response = new MovieDto();
+
+    response.movie = movie;
+
+    return response;
+  }
+
+  /**
+   * PUT /movies/:id route handler
+   *
+   * @param {Movie} body
+   * @return {Promise<MovieDto>}
+   */
+  @Put(":id")
+  async update(
+    @Param("id") id: number,
+    @Body() body: Movie
+  ): Promise<MovieDto> {
+    const movie = await this.movieService.findOne(id);
+
+    if (!movie) {
+      throw new HttpException("Movie not found", HttpStatus.NOT_FOUND);
+    }
+
+    const response = new MovieDto();
+
+    response.movie = await this.movieService.update(id, body);
 
     return response;
   }
--- src/modules/movies/service.ts
+++ src/modules/movies/service.ts
@@ -3,17 +3,34 @@ import { Repository } from "typeorm";
 import { PaginationDto } from "../../dto/pagination.dto";
 import { Movie } from "../../entity/Movie";
 
+/**
+ * Contains methods to perform CRUD operations on Movie entities
+ */
 @Injectable()
 class MovieService {
+  /**
+   * @param {Repository<Movie>} movieRepository
+   */
   constructor(
     @Inject("MOVIE_REPOSITORY")
     private movieRepository: Repository<Movie>
   ) {}
 
+  /**
+   * Returns the number of movie records currently in the repository
+   *
+   * @return {Promise<number>}
+   */
   async totalRecords(): Promise<number> {
     return this.movieRepository.count();
   }
 
+  /**
+   * Returns all records for the paginated set
+   *
+   * @param {PaginationDto} query
+   * @return {Promise<Movie[]>}
+   */
   async findAll(query: PaginationDto): Promise<Movie[]> {
     return this.movieRepository
       .createQueryBuilder()
@@ -22,10 +39,43 @@ class MovieService {
       .getMany();
   }
 
+  /**
+   * Returns the movie using the provided identifier
+   *
+   * @param {number} id
+   * @return {Promise<Movie>}
+   */
   async findOne(id: number): Promise<Movie | undefined> {
     return this.movieRepository.findOne(id, {
       relations: ["genres", "keywords", "companies"],
     });
+  }
+
+  /**
+   * Creates a movie using the provided payload
+   *
+   * @param {Movie} movie
+   * @return {Promise<Movie>}
+   */
+  async create(movie: Movie): Promise<Movie> {
+    return this.movieRepository.save(movie);
+  }
+
+  /**
+   * Updates an existing movie using the provided id and payload
+   *
+   * @param {Movie} movie
+   * @return {Promise<Movie>}
+   */
+  async update(id: number, movie: Movie): Promise<Movie> {
+    await this.movieRepository
+      .createQueryBuilder()
+      .update(Movie)
+      .set(movie)
+      .where("id = :id", { id })
+      .execute();
+
+    return (this.movieRepository.findOne(id) as unknown) as Movie;
   }
 }
 
