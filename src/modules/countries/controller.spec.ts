8883a633dfe4723cc9b57bb6a2356258724b2a7a
Kyle <kylefredin@gmail.com>
2021-02-21 13:06:30 -0700

    add more tests

--- src/modules/countries/controller.spec.ts
+++ src/modules/countries/controller.spec.ts
@@ -1,4 +1,6 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
+import { CountryDto } from "../../dto/country.dto";
 import { CountriesDto } from "../../dto/countries.dto";
 import { PaginationDto } from "../../dto/pagination.dto";
 import { Country } from "../../entity/Country";
@@ -70,6 +72,71 @@ describe("CountryController", () => {
       response.meta.perPage = query.perPage;
 
       expect(await countryController.findAll(query)).toStrictEqual(response);
+    });
+  });
+
+  describe("findOne", () => {
+    it("should throw an error if id is not found", () => {
+      jest.spyOn(countryService, "findOne").mockResolvedValue(undefined);
+
+      expect(async () => countryController.findOne(0)).rejects.toThrowError(
+        new HttpException("Country not found", HttpStatus.NOT_FOUND),
+      );
+    });
+
+    it("should return the found country", async () => {
+      const country = { id: 1, isoCode: "AB", name: "Test" };
+
+      jest.spyOn(countryService, "findOne").mockResolvedValue(country);
+
+      const response = new CountryDto();
+      response.country = country;
+
+      expect(await countryController.findOne(1)).toStrictEqual(response);
+    });
+  });
+
+  describe("create", () => {
+    it("should return the created country", async () => {
+      const countryRequest = { isoCode: "AB", name: "Test" } as Country;
+      const countryResponse = { id: 1, isoCode: "AB", name: "Test" };
+
+      jest.spyOn(countryService, "create").mockResolvedValue(countryResponse);
+
+      const response = new CountryDto();
+      response.country = countryResponse;
+
+      expect(await countryController.create(countryRequest)).toStrictEqual(
+        response,
+      );
+    });
+  });
+
+  describe("update", () => {
+    it("should throw an error if id is not found", () => {
+      const country = { id: 1, isoCode: "AB", name: "Test" };
+
+      jest.spyOn(countryService, "findOne").mockResolvedValue(undefined);
+
+      expect(async () =>
+        countryController.update(0, country),
+      ).rejects.toThrowError(
+        new HttpException("Country not found", HttpStatus.NOT_FOUND),
+      );
+    });
+
+    it("should return the updated country", async () => {
+      const country = { id: 1, isoCode: "AB", name: "Test" };
+
+      jest.spyOn(countryService, "findOne").mockResolvedValue(country);
+      jest.spyOn(countryService, "update").mockResolvedValue(country);
+
+      const response = new CountryDto();
+      response.country = country;
+
+      expect(await countryController.update(1, country)).toStrictEqual(
+        response,
+      );
     });
   });
 });
